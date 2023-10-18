0ae16a17d043ae697a17cc7789c267b34ac8b375
ewilkinsiii <ewilkinsiii@gmail.com>
2023-10-18 16:52:36 -0500

    Created post migration and seeder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_18_010943) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_18_211718) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
@@ -37,4 +46,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_010943) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
