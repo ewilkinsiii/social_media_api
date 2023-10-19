62961dd6cb67b20ca65c35db2eec1173e8be78ec
ewilkinsiii <ewilkinsiii@gmail.com>
2023-10-19 13:34:52 -0500

    Added average_rating to user Serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :first_name, :last_name, :avatar
+  attributes :id, :username, :email, :first_name, :last_name, :avatar, :average_rating
   has_many :posts
+
+  def average_rating
+    UserRating.average_rating(object.id)
+  end
 end
