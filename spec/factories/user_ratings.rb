e19d0c6d5a81e8f8c33423b6a9176f4833c82c61
ewilkinsiii <ewilkinsiii@gmail.com>
2023-10-21 13:20:50 -0500

    Created user rating response test

--- spec/factories/user_ratings.rb
+++ spec/factories/user_ratings.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user_rating do
     rating { Faker::Number.between(from: 1, to: 5) }
-    user { create(:user) }
+    user_id { create(:user).id }
     rater_id { create(:user).id }
   end
 end
